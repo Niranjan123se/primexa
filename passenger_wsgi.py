cd79161e1eb690f0208adc4272be2fe509b4ca9d
nir <nir@gmail.com>
2026-09-21 14:58:28 +0530

    change in requiremnet txt

---
+++ passenger_wsgi.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+# -----------------------------------------------------------------------------
+# cPanel Passenger WSGI Entrypoint for Primexa Exchange (Python 3.13)
+# -----------------------------------------------------------------------------
+
+# Add project root directory to python path
+sys.path.insert(0, os.path.dirname(__file__))
+
+# Set Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "primexa_project.settings")
+
+# Import WSGI handler
+from primexa_project.wsgi import application
